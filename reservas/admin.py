679d667b7db2287373dbe01c09aedbb3aeecdd61
PhilippeDMG <philippedamau@gmail.com>
2026-05-03 20:29:44 -0300

    add create user from admin panel

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 from .models import Cargo, Usuario, Vehiculo, Ticket
 
 
@@ -8,12 +9,34 @@ class CargoAdmin(admin.ModelAdmin):
     ordering = ("prioridad",)
 
 
+class UsuarioForm(forms.ModelForm):
+    contrasena = forms.CharField(
+        widget=forms.PasswordInput,
+        required=False,
+        label="Contraseña",
+        help_text="Dejar en blanco para mantener la contraseña actual."
+    )
+
+    class Meta:
+        model = Usuario
+        fields = "__all__"
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        password = self.cleaned_data.get("contrasena")
+        if password:
+            instance.set_password(password)
+        if commit:
+            instance.save()
+        return instance
+
+
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
+    form = UsuarioForm
     list_display = ("nombre_completo", "correo", "id_cargo", "valido", "rechazado")
     list_filter = ("valido", "rechazado", "id_cargo")
     search_fields = ("nombre", "apellido", "correo")
-    readonly_fields = ("contrasena",)
 
     def nombre_completo(self, obj):
         return obj.nombre_completo
